9cb6500c172e7f055df8f98ac6be1730eb85a6fd
Documenter.jl <documenter@juliadocs.github.io>
2024-11-18 10:50:42 +0000

    build based on e87cb6c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GenomicCoordinates","category":"page"},{"location":"#GenomicCoordinates","page":"Home","title":"GenomicCoordinates","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GenomicCoordinates.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GenomicCoordinates]","category":"page"}]
+}
